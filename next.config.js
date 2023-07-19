a0470dfb2a1c8dc0d1275ee3767e4180feae6dc3
SerRat44 <narlytoes@gmail.com>
2023-07-19 16:41:33 -0700

    stuff

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
+const prod = process.env.NODE_ENV === "production";
+
+
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '',
+  assetPrefix: prod ? './' : '',
+  output: 'export',
 }
 
 module.exports = nextConfig
