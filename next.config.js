e1f03bac620aff725bdf0de2fea0f7fb7cb51170
SerRat44 <narlytoes@gmail.com>
2023-07-19 13:08:29 -0700

    stuff

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '',
 }
 
 module.exports = nextConfig
